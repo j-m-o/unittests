2f777615334956a56c1b20e7792c50fb6f873f3c
j-m-o <42677462+j-m-o@users.noreply.github.com>
2019-11-07 13:15:23 -0600

    Simple Unit Test

---
+++ JessTestProject/JessTestProject/CalculatorTest.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JessTestProject
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Adding_4_And_3_Should_Return_7()
+        {
+            var calculator = new Calculator();
+
+            int result = calculator.Add(4, 3);
+
+            Assert.AreEqual(7, result);
+        }
+    }
+}
